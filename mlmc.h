bc1b09a7cb5752652b18d8fc7294459cb9cd2505
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-12-07 14:37:55 +0000

    Added interface structure

---
+++ mlmc.h
@@ -0,0 +1,64 @@
+#ifndef __MLMC_H__
+#define __MLMC_H__
+
+typedef struct s_mlmc_options{
+    unsigned int Lmin;  // Minimum number of levels
+    unsigned int Lmax;  // Maximum number of levels
+
+    unsigned long long N0;   // Initial number of samples to compute variance estimates
+
+    double alpha;             // Weak convergence rate. Must be positive
+    double beta;              // Variance convergence rate. Must be positive
+    double gamma;             // Work rate. Must be positive
+
+    // It will be difficult to support variable length vectors efficiently.
+    // I think we should focus on fixed length vectors for now.
+    unsigned int per_sample;  // Vector length for vector quantities
+
+    void *user_data;
+} mlmc_options;
+
+typedef struct s_mlmc_output{
+    unsigned int L;
+    unsigned int moments;
+    unsigned int per_moment;
+
+    unsigned long long* M;   // Size L
+    double* sums;            // Size L*moments*per_moments
+}mlmc_output;
+
+typedef void fn_mlmc_sample_levels(unsigned int L,
+                                   const unsigned long long* M,
+                                   double *sums,
+                                   unsigned int sums_size,
+                                   void *user_data);
+
+typedef void fn_mlmc_sample_level(unsigned int ell,
+                                  unsigned long long M,
+                                  double *sums,
+                                  unsigned int sums_size,
+                                  void *user_data);
+
+mlmc_options* MLMCCreateOptions();
+void MLMCFreeOptions(mlmc_options*);
+
+mlmc_output* MLMCRun(double eps, fn_mlmc_sample_levels sampler,
+                     const mlmc_options*); // Options can be NULL for default
+void MLMCFreeOutput(mlmc_options*);
+
+// This function call a simple single-level sampler
+// for every level.
+void MLMCSampleLevels(fn_mlmc_sample_level lvl_sampler,
+                      unsigned int L,
+                      const unsigned long long* M,
+                      double *sums,
+                      unsigned int sums_size,
+                      void *user_data);
+
+// More functions to manipulate output of MLMCRun
+void MLMCEstimateError(const mlmc_output* error, double* discretization_error,
+                       double *stat_error);
+void MLMCComputeEstimate(const mlmc_output* error,
+                         double *estimate,
+                         unsigned int estimate_max_size);
+#endif   // __MLMC_H__
