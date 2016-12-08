35856496baf5d8b7a5a7c69985343d8ec1118be8
David Warne <david.warne@qut.edu.au>
2016-12-09 09:24:09 +1000

    Modified mlmc_options
    Single level and all level function typdefs have been move to be
    function pointer members of the mlmc_options type.

--- mlmc.h
+++ mlmc.h
@@ -15,6 +15,20 @@ typedef struct s_mlmc_options{
     // I think we should focus on fixed length vectors for now.
     unsigned int per_sample;  // Vector length for vector quantities
 
+    // user supplied function to sample all levels
+    void (*fn_mlmc_sample_levels)(unsigned int L,
+                                   const unsigned long long* M,
+                                   double *sums,
+                                   unsigned int sums_size,
+                                   void *user_data);
+
+    // user supplied function to sample a single level
+    void (*fn_mlmc_sample_level)(unsigned int ell,
+                                  unsigned long long M,
+                                  double *sums,
+                                  unsigned int sums_size,
+                                  void *user_data);
+   
     void *user_data;
 } mlmc_options;
 
@@ -27,28 +41,23 @@ typedef struct s_mlmc_output{
     double* sums;            // Size L*moments*per_moments
 }mlmc_output;
 
-typedef void fn_mlmc_sample_levels(unsigned int L,
-                                   const unsigned long long* M,
-                                   double *sums,
-                                   unsigned int sums_size,
-                                   void *user_data);
 
-typedef void fn_mlmc_sample_level(unsigned int ell,
-                                  unsigned long long M,
-                                  double *sums,
-                                  unsigned int sums_size,
-                                  void *user_data);
+mlmc_options* 
+MLMCCreateOptions();
 
-mlmc_options* MLMCCreateOptions();
-void MLMCFreeOptions(mlmc_options*);
+void 
+MLMCFreeOptions(mlmc_options*);
 
-mlmc_output* MLMCRun(double eps, fn_mlmc_sample_levels sampler,
-                     const mlmc_options*); // Options can be NULL for default
-void MLMCFreeOutput(mlmc_options*);
+mlmc_output* 
+MLMCRun(double eps, const mlmc_options*); // Options can be NULL for default
+
+void 
+MLMCFreeOutput(mlmc_options*);
 
 // This function call a simple single-level sampler
 // for every level.
-void MLMCSampleLevels(fn_mlmc_sample_level lvl_sampler,
+void 
+MLMCSampleLevels(const mlmc_options*,
                       unsigned int L,
                       const unsigned long long* M,
                       double *sums,
@@ -56,9 +65,12 @@ void MLMCSampleLevels(fn_mlmc_sample_level lvl_sampler,
                       void *user_data);
 
 // More functions to manipulate output of MLMCRun
-void MLMCEstimateError(const mlmc_output* error, double* discretization_error,
+void 
+MLMCEstimateError(const mlmc_output* error, double* discretization_error,
                        double *stat_error);
-void MLMCComputeEstimate(const mlmc_output* error,
+
+void 
+MLMCComputeEstimate(const mlmc_output* error,
                          double *estimate,
                          unsigned int estimate_max_size);
 #endif   // __MLMC_H__
