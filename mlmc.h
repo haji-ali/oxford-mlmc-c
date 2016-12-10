48dc6eeb65dbb8eb6553ee726965e55b05048eff
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-12-10 11:35:18 +0000

    Cleaning up and making the header-file more GNU styled

--- mlmc.h
+++ mlmc.h
@@ -1,6 +1,18 @@
 #ifndef __MLMC_H__
 #define __MLMC_H__
 
+typedef void (*fn_mlmc_sample_levels_t)(unsigned int L,
+                                        const unsigned long long* M,
+                                        double *sums,
+                                        unsigned int sums_size,
+                                        void *user_data);
+typedef void (*fn_mlmc_sample_level_t)(unsigned int ell,
+                                       unsigned long long M,
+                                       double *sums,
+                                       unsigned int sums_size,
+                                       void *user_data);
+
+
 typedef struct s_mlmc_options{
     unsigned int Lmin;  // Minimum number of levels
     unsigned int Lmax;  // Maximum number of levels
@@ -16,19 +28,10 @@ typedef struct s_mlmc_options{
     unsigned int per_sample;  // Vector length for vector quantities
 
     // user supplied function to sample all levels
-    void (*fn_mlmc_sample_levels)(unsigned int L,
-                                   const unsigned long long* M,
-                                   double *sums,
-                                   unsigned int sums_size,
-                                   void *user_data);
-
+    fn_mlmc_sample_levels_t fn_mlmc_sample_levels;
     // user supplied function to sample a single level
-    void (*fn_mlmc_sample_level)(unsigned int ell,
-                                  unsigned long long M,
-                                  double *sums,
-                                  unsigned int sums_size,
-                                  void *user_data);
-   
+    fn_mlmc_sample_level_t fn_mlmc_sample_level;
+
     void *user_data;
 } mlmc_options;
 
@@ -43,50 +46,46 @@ typedef struct s_mlmc_output{
 
 // commandline like options creation
 mlmc_options*
-MLMCCreateOptionsForCLI(int argc, char ** argv);
+mlmc_create_option_cli(int argc, char ** argv);
 
 // explicit MLMC options creation
-mlmc_options* 
-MLMCCreateOptions(unsigned int Lmin, unsigned int LMax,
-    unsigned int N0, double alpha, double beta, double gamma,
-    unsigned int per_sample, 
-    void (*fn_mlmc_sample_levels)(unsigned int L,
-                                   const unsigned long long* M,
-                                   double *sums,
-                                   unsigned int sums_size,
-                              void *user_data),
-    void (*fn_mlmc_sample_level)(unsigned int ell,
-                                  unsigned long long M,
-                                  double *sums,
-                                  unsigned int sums_size,
-                                  void *user_data));
+mlmc_options*
+mlmc_create_option(unsigned int Lmin, unsigned int LMax,
+                   unsigned int N0, double alpha, double beta, double gamma,
+                   unsigned int per_sample,
+                   fn_mlmc_sample_levels_t fn_mlmc_sample_levels);
 
-void 
-MLMCFreeOptions(mlmc_options*);
+mlmc_options*
+mlmc_create_option_simple(unsigned int Lmin, unsigned int LMax,
+                          unsigned int N0, double alpha, double beta, double gamma,
+                          unsigned int per_sample,
+                          fn_mlmc_sample_level_t fn_mlmc_sample_level);
+void
+mlmc_free_options(mlmc_options*);
 
-mlmc_output* 
-MLMCRun(double eps, const mlmc_options*); // Options can be NULL for default
+mlmc_output*
+mlmc_run(double eps, const mlmc_options*); // Options can be NULL for default
 
-void 
-MLMCFreeOutput(mlmc_options*);
+void
+mlmc_free_output(mlmc_options*);
 
-// This function call a simple single-level sampler
-// for every level.
-void 
-MLMCSampleLevels(const mlmc_options*,
-                      unsigned int L,
-                      const unsigned long long* M,
-                      double *sums,
-                      unsigned int sums_size,
-                      void *user_data);
+// This function calls a simple single-level sampler for every level.
+void
+mlmc_sample_levels(const mlmc_options*,
+                   unsigned int L,
+                   const unsigned long long* M,
+                   double *sums,
+                   unsigned int sums_size,
+                   void *user_data);
 
 // More functions to manipulate output of MLMCRun
-void 
-MLMCEstimateError(const mlmc_output* error, double* discretization_error,
-                       double *stat_error);
+void
+mlmc_estimate_error(const mlmc_output* error, double* discretization_error,
+                    double *stat_error);
 
-void 
-MLMCComputeEstimate(const mlmc_output* error,
-                         double *estimate,
-                         unsigned int estimate_max_size);
+void
+mlmc_compute_estimate(const mlmc_output* error,
+                      double *estimate,
+                      unsigned int estimate_max_size);
+
 #endif   // __MLMC_H__
