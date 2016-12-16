e28ca79504d71e32a45288257415cf5b17e7ca3c
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-12-16 10:32:10 +0000

    Finished algorithm implementation

--- mlmc.c
+++ mlmc.c
@@ -13,6 +13,9 @@
 
 typedef int bool;
 
+/**
+ * @copydoc mlmc_create_option
+ */
 mlmc_options*
 mlmc_create_option(double eps, fn_mlmc_sample_levels_t fn_mlmc_sample_levels)
 {
@@ -27,6 +30,9 @@ mlmc_create_option(double eps, fn_mlmc_sample_levels_t fn_mlmc_sample_levels)
     return opt;
 }
 
+/**
+ * @copydoc mlmc_create_option_simple
+ */
 mlmc_options*
 mlmc_create_option_simple(double eps, fn_mlmc_sample_level_t fn_mlmc_sample_level)
 {
@@ -43,12 +49,18 @@ mlmc_create_option_simple(double eps, fn_mlmc_sample_level_t fn_mlmc_sample_leve
     return opt;
 }
 
+/**
+ * @copydoc mlmc_free_options
+ */
 void
 mlmc_free_options(mlmc_options* opt)
 {
     free(opt);
 }
 
+/**
+ * @copydoc mlmc_free_output
+ */
 void
 mlmc_free_output(mlmc_output* out)
 {
@@ -58,7 +70,13 @@ mlmc_free_output(mlmc_output* out)
     free(out);
 }
 
-void regression(int N, double *x, double *y, double *a, double *b){
+/**
+ * Fits a model f(x) = b*x^a given approximations of y
+ * @param N Number of points
+ */
+void
+regression(int N, const double *x, const double *y, double *a, double *b)
+{
 
     float sum0=0.0f, sum1=0.0f, sum2=0.0f, sumy0=0.0f, sumy1=0.0f;
 
@@ -75,26 +93,31 @@ void regression(int N, double *x, double *y, double *a, double *b){
     *b = (sum2*sumy0 - sum1*sumy1) / (sum0*sum2 - sum1*sum1);
 }
 
+/**
+ * Internal function to computes samples
+ * @param L Number of levels
+ * @param opt
+ * @param dNl Number of required samples on each level.
+ * @param out
+ * @note After calling this function, each element in out->N should be at
+ * greater or equal to the corresponding element dNl.
+ */
 void
 mlmc_run_all_levels(unsigned int L,
-    const mlmc_options* opt, const unsigned long long *dNl,
-    mlmc_output *out)
+                    const mlmc_options* opt, const unsigned long long *dNl,
+                    mlmc_output *out)
 {
     if (opt->fn_mlmc_sample_levels)
     {
         // Multi-level mode
-        double *sums_moments = malloc(sizeof(double)*opt->Lmax*
-                                      opt->per_sample*MLMC_MOMENTS_COUNT);
+        double *sums_moments = malloc(sizeof(double)*opt->Lmax
+                                      *opt->per_sample*MLMC_MOMENTS_COUNT);
         double *sums_work = malloc(sizeof(double)*opt->Lmax);
-        unsigned long long *dN_done = malloc(sizeof(unsigned int)*L*
-                                       opt->per_sample*MLMC_MOMENTS_COUNT);
-        unsigned long long *dN_todo = malloc(sizeof(unsigned int)*L*
-                                       opt->per_sample*MLMC_MOMENTS_COUNT);
+        unsigned long long *dN_todo = malloc(sizeof(unsigned int)*L
+                                             *opt->per_sample
+                                             *MLMC_MOMENTS_COUNT);
         for (unsigned int l=0;l<L;l++)
-        {
-            dN_done[l] = 0;
-            dN_todo[l] = dNl[0];
-        }
+            dN_todo[l] = dNl[l]>out->Nl[l] ? dNl[l] - out->Nl[l] : 0 ;
 
         while (1){
             opt->fn_mlmc_sample_levels(L, dN_todo, sums_moments,
@@ -104,9 +127,14 @@ mlmc_run_all_levels(unsigned int L,
             unsigned int sum=0;
             for (unsigned int l=0;l<L;l++)
             {
-                dN_done[l] += dN_todo[l];
-                // TODO: Assimilate into output
-                dN_todo[l] = dNl[l]>dN_done[l] ? dNl[l] - dN_done[l] : 0 ;
+                for(unsigned int n=0;n<opt->per_sample*MLMC_MOMENTS_COUNT;n++)
+                    out->sums_moments[n + l*opt->per_sample*MLMC_MOMENTS_COUNT]
+                        += out->sums_moments[n+l*opt->per_sample
+                                             *MLMC_MOMENTS_COUNT];
+                out->Nl[l] += dN_todo[l];
+                out->sums_work[l] += sums_work[l];
+
+                dN_todo[l] = dNl[l]>out->Nl[l] ? dNl[l] - out->Nl[l] : 0 ;
                 sum += dN_todo[l];
             }
             if (sum == 0)
@@ -118,26 +146,33 @@ mlmc_run_all_levels(unsigned int L,
     else
     {
         // Single level mode
-        double *sums_moments = malloc(sizeof(double)*
-                                      opt->per_sample*MLMC_MOMENTS_COUNT);
+        double *sums_moments = malloc(sizeof(double)
+                                      *opt->per_sample*MLMC_MOMENTS_COUNT);
         double sums_work;
         for (unsigned int l=0;l<L;l++){
             unsigned long long dN_todo = dNl[l];
-            unsigned long long dN_done = 0;
             while(dN_todo > 0){
                 opt->fn_mlmc_sample_level(l, &dN_todo, sums_moments,
                                           opt->per_sample*MLMC_MOMENTS_COUNT,
                                           &sums_work, opt->user_data);
-                dN_done += dN_todo;
-                // TODO: Assimilate into output
+                // Assimilate into output
+                for(unsigned int n=0;n<opt->per_sample*MLMC_MOMENTS_COUNT;n++)
+                    out->sums_moments[n + l*opt->per_sample*MLMC_MOMENTS_COUNT]
+                        += out->sums_moments[n+l*opt->per_sample
+                                             *MLMC_MOMENTS_COUNT];
+                out->Nl[l] += dN_todo;
+                out->sums_work[l] += sums_work;
 
-                dN_todo = dNl[l]>dN_done ? dNl[l] - dN_done : 0 ;
+                dN_todo = dNl[l]>out->Nl[l] ? dNl[l] - out->Nl[l] : 0 ;
             }
         }
         free(sums_moments);
     }
 }
 
+/**
+ * @copydoc mlmc_run
+ */
 mlmc_output*
 mlmc_run(const mlmc_options* opt)
 {
@@ -157,7 +192,7 @@ mlmc_run(const mlmc_options* opt)
     unsigned int L = opt->Lmin;
     bool converged = 0;
 
-    unsigned long long *dNl = malloc(sizeof(long long) * (Lmax+1));
+    unsigned long long *Nl = malloc(sizeof(long long) * (Lmax+1));
     double *El = malloc(sizeof(double) * (Lmax+1));
     double *Vl = malloc(sizeof(double) * (Lmax+1));
     double *Cl = malloc(sizeof(double) * (Lmax+1));
@@ -171,27 +206,28 @@ mlmc_run(const mlmc_options* opt)
     bool diag = 1;
 
     for(unsigned int l=0; l<=Lmax; l++) {
-        dNl[l]  = 0;
+        Nl[l]  = 0;
         Cl[l]   = powf(2.0f,(float)l*gamma);
         for(unsigned int n=0; n<opt->per_sample * MLMC_MOMENTS_COUNT; n++)
             out->sums_moments[n + l*opt->per_sample * MLMC_MOMENTS_COUNT] = 0.0;
+        out->Nl[l] = 0;
+        out->sums_work[l] = 0;
     }
 
     for(int l=0; l<=opt->Lmin; l++)
-        dNl[l] = opt->N0;
+        Nl[l] = opt->N0;
 
     while (!converged){
         //
         // update sample sums
         //
-        mlmc_run_all_levels(L, opt, dNl, out);
+        mlmc_run_all_levels(L, opt, Nl, out);
 
         //
         // compute absolute average, variance and cost,
         // correct for possible under-sampling,
         // and set optimal number of new samples
         //
-
         double sum = 0.0f;
         for (unsigned int l=0; l <= Lmax; l++) {
             if (!out->Nl[l])     // No samples on this level
@@ -226,10 +262,9 @@ mlmc_run(const mlmc_options* opt)
         }
 
         for (unsigned int l=0; l<=Lmax; l++) {
-            dNl[l] = ceilf(fmaxf(0.0f,
+            Nl[l] = ceilf(fmaxf(0.0f,
                                  sqrtf(Vl[l]/Cl[l])*sum
-                                 / ((1.0f-opt->theta)*opt->eps*opt->eps)
-                                 - out->Nl[l] ));
+                                 / ((1.0f-opt->theta)*opt->eps*opt->eps)));
         }
 
         //
@@ -263,7 +298,7 @@ mlmc_run(const mlmc_options* opt)
         //
         sum = 0.0;
         for (int l=0; l<=Lmax; l++)
-            sum += fmaxf(0.0f, (double)dNl[l]-0.01*out->Nl[l]);
+            sum += fmaxf(0.0f, (double)Nl[l]-out->Nl[l]);
 
         if (sum == 0) {
             if (diag) printf(" achieved variance target \n");
@@ -285,9 +320,10 @@ mlmc_run(const mlmc_options* opt)
                     sum = 0.0f;
                     for (int l=0; l<=L; l++) sum += sqrtf(Vl[l]*Cl[l]);
                     for (int l=0; l<=L; l++)
-                        dNl[l] = ceilf( fmaxf( 0.0f,
-                                               sqrtf(Vl[l]/Cl[l])*sum/((1.0f-opt->theta)*opt->eps*opt->eps)
-                                               - out->Nl[l] ) );
+                        Nl[l] = ceilf( fmaxf( 0.0f,
+                                               sqrtf(Vl[l]/Cl[l])*sum
+                                               /((1.0f-opt->theta)*opt->eps
+                                                 *opt->eps)) );
                 }
             }
         }
